fcae72a9afb6b59eb6eb1d7194150f00f6130ebe
Tom Fuertes <tomfuertes@gmail.com>
2015-08-11 11:12:52 -0500

    notifyjs

---
+++ src/notify.js
@@ -0,0 +1,34 @@
+/**
+ * @desc notify()
+ * - http://notifyjs.com/ for examples / docs
+ *
+ * @param {Function} fn - the function to curry
+ * @param {Number} [len] - specifies the number of arguments needed to call the function
+ *
+ * @return {Function} - the curried function
+ */
+
+const url = '//rawgit.com/clearhead/clearhead/master/bower_components/notifyjs/dist/notify.min.js';
+let script = null;
+
+function notify(...args) {
+
+  // only notify in debug mode
+  if (!/clearhead-debug=true/.test(document.cookie)) {
+    return;
+  }
+
+  // wait for jQuery
+  if (!window.jQuery) {
+    return setTimeout(notify.bind(this, ...args), 1000);
+  }
+
+  script = script || jQuery.getScript(url); // promise
+
+  script.done(function () {
+    $.notify.apply($, args.join(': '));
+  });
+
+}
+
+export default notify;
---
+++ test/notify.js
@@ -0,0 +1,10 @@
+import chai, { assert, expect } from 'chai';
+import curry from '../src/notify';
+
+describe('#notify()', () => {
+
+  it('should notify a thing', () => {
+    assert.equal('tomf', 'good person');
+  });
+
+});
