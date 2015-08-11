780b80f91d1dcf53a8149387d96508cc3a04807f
Tom Fuertes <tomfuertes@gmail.com>
2015-08-11 15:53:08 -0500

    bower install domready

---
+++ src/domready.js
@@ -0,0 +1,9 @@
+/**
+ * @desc domready() runs a function on domready
+ *
+ * @param {Function} fn - the function to run on domready
+ *
+ * @return {undefined}
+ */
+
+export default require('../bower_components/domready/ready.min.js');
