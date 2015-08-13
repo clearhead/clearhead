9e3fe388cb1c7880089f645215ab0e3d95cee2ea
Tom Fuertes <tomfuertes@gmail.com>
2015-08-13 09:44:12 -0500

    log old test for load-script

--- test/load-script.js
+++ test/load-script.js
@@ -8,3 +8,29 @@ describe('#loadScript()', () => {
   });
 
 });
+
+// TODO: https://github.com/clearhead/optimizely-snippets/blob/master/tests/LoadScriptSpec.js
+// describe('experiment/load-script.js', function() {
+//   var script, firstScript, onload = function () {};
+//   beforeEach(function() {
+//     script = {};
+//     firstScript = {
+//       parentNode: {
+//         insertBefore: function () {}
+//       }
+//     };
+//     spyOn(document, 'createElement').and.returnValue(script);
+//     spyOn(document, 'getElementsByTagName').and.returnValue([firstScript]);
+//     spyOn(firstScript.parentNode, 'insertBefore');
+//   });
+//   if ('should load a script w/ an onload callback', function() {
+//       clearhead.loadScript('//demo', onload);
+//       expect(document.createElement).toHaveBeenCalledWith('script');
+//       expect(document.getElementsByTagName).toHaveBeenCalledWith('script');
+//       expect(script.type).toBe('text/javascript');
+//       expect(script.async).toBe(true);
+//       expect(script.src).toBe('demo');
+//       expect(script.onload).toBe(onload);
+//       expect(firstScript.parentNode.insertBefore).toHaveBeenCalledWith(script, firstScript);
+//     });
+// });
