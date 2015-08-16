f9411c3685216c7d6c10c9e083cd214f292b1bdc
Tom Fuertes <tomfuertes@gmail.com>
2015-08-16 10:59:09 -0500

    check for jQuery

--- src/when.js
+++ src/when.js
@@ -5,12 +5,24 @@
 import log from './log';
 
 function when(selector, callback, optTimeout) {
-  var $this = typeof jQuery === 'function' ? jQuery(selector) : [];
+
+  // sniff for jQuery in local namespace
+  var $jq = (
+    typeof jQuery === 'function' ? jQuery : (
+      (typeof $ === 'function' && $.fn && $.fn.jquery) ? $ : undefined
+    )
+  );
+
+  // search dom for element
+  var $this = typeof $jq === 'function' ? jQuery(selector) : [];
+
   log('when:', selector, $this);
+
   return $this.length ? callback.call($this, $this) : setTimeout(
     when.bind(null, selector, callback, optTimeout),
     optTimeout || 50
   );
+
 }
 
 export default when;
