97975a4253e9686bd3693785746a892a0d87c594
Tom Fuertes <tomfuertes@gmail.com>
2015-08-17 12:54:58 -0500

    can't require in build

--- src/domready.js
+++ src/domready.js
@@ -6,4 +6,27 @@
  * @return {undefined}
  */
 
-export default require('../bower_components/domready/ready.min.js');
+// require('../bower_components/domready/ready.min.js');
+function domready() {
+
+  var fns = [],
+    listener, doc = document,
+    hack = doc.documentElement.doScroll,
+    domContentLoaded = 'DOMContentLoaded',
+    loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+
+
+  if (!loaded)
+    doc.addEventListener(domContentLoaded, listener = function() {
+      doc.removeEventListener(domContentLoaded, listener)
+      loaded = 1
+      while (listener = fns.shift()) listener()
+    })
+
+  return function(fn) {
+    loaded ? setTimeout(fn, 0) : fns.push(fn)
+  }
+
+}
+
+export default domready;
