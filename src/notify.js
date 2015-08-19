11278da716c79cc555a75c94c363b1c1e1d8dc0b
Tom Fuertes <TomFuertes@gmail.com>
2015-08-19 15:58:14 -0500

    Update notify.js

--- src/notify.js
+++ src/notify.js
@@ -26,7 +26,7 @@ function notify(...args) {
   script = script || jQuery.getScript(url); // promise
 
   script.done(function () {
-    $.notify.apply($, args.join(': '));
+    jQuery.notify.apply(jQuery, args.join(': '));
   });
 
 }
