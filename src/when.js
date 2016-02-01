a968969d67418bddd2fc13f719924c746d55e0b0
Beau <beau@clearhead.me>
2016-02-01 14:16:58 -0600

    Removed the timeout parameters and added the abilty to stop the When loop

--- src/when.js
+++ src/when.js
@@ -2,7 +2,11 @@
  * @desc when() polls or an jQuery && an element
  * You can disable the log call by adding silentWhen=true as a query parameter to the page
  * or creating a silentWhen variable on the window and setting it to true;
+ *
+ * @param {string} [selector] - Any valid jQuery selector OR 'stop' which will stop all polling
+ * @param {function|array} [callback] - A callback function to run once the selected element has been found OR a list of jQuery things to do i.e. when('#container', 'css', 'color', 'red');
  */
+
 import log from './log';
 import getParam from './get-param';
 
@@ -10,21 +14,21 @@ import getParam from './get-param';
 var mainLoop = null;
 var elements = [];
 
-function when(selector, callback, optTimeout, optQuitPolling) {
+function when(selector, callback) {
+  if(selector === 'stop') {
+    clearInterval(mainLoop);
+  }
+
+  //Convert the list of supplied arguments into a true array (since it's actually an object)
+  var args = (arguments.length === 1?[arguments[0]]:Array.apply(null, arguments));
+
   //Add the new element and it's associated arguments to the list of things to look check for.
-  elements.push(arguments);
+  elements.push(args);
 
   //If this is the first time when has been called
   if(mainLoop === null) {
     //Start a loop that checks for elements
     mainLoop = setInterval(function() {
-      //Quit if we've gone past the optQuitPolling time
-      if(optQuitPolling <= 0) {
-        clearInterval(mainLoop);
-        log('When stopped polling');
-        return false;
-      }
-
       //Sniff for jQuery in local namespace
       var $jq = (
         typeof jQuery === 'function' ? jQuery : (
@@ -36,6 +40,7 @@ function when(selector, callback, optTimeout, optQuitPolling) {
         //Just some reference variables to make the code easier to read
         var xSelector = elements[i][0];
         var xCallback = elements[i][1];
+        var xArguments = elements[i].slice(2);
 
         //Search the dom for the element
         var $this = typeof $jq === 'function' ? $jq(xSelector) : [];
@@ -54,9 +59,9 @@ function when(selector, callback, optTimeout, optQuitPolling) {
             xCallback.call($this, $this);
           }
           //If the supplied callback was actually an array that looks like a jQuery method
-          else if(xCallback.constructor === Array && xCallback.length === 2 && xCallback[1].constructor === Array) {
+          else if(typeof xCallback === 'string') {
             //Run that on the element we just found
-            $this[xCallback[0]].apply($this, xCallback[1]);
+            $this[xCallback].apply($this, xArguments);
           }
           //Stop looking for this element
           elements.splice(i, 1);
@@ -67,10 +72,7 @@ function when(selector, callback, optTimeout, optQuitPolling) {
       if(elements.length < 1) {
         clearInterval(mainLoop);
       }
-
-      //Reduce the time left to check for the element
-      optQuitPolling = optQuitPolling - (optTimeout || 50);
-    }, (optTimeout || 50));
+    }, 50);
   }
 }
 
