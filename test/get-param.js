5093786f3fdc57ac0a71bd29f1f90ad8846eb9f5
Casey White <casey.r.white@gmail.com>
2016-03-25 10:35:36 -0500

    setup test skeleton

--- test/get-param.js
+++ test/get-param.js
@@ -1,17 +1,10 @@
 import chai, { assert, expect } from 'chai';
-import curry from '../src/get-param';
+import getParam from '../src/get-param';
 
 describe('#getParam()', () => {
 
-  it('should getParam a thing', () => {
-    assert.equal('tomf', 'tomf'); // 'good person');
+  it('should return the value of a key given a URL string with query parameters', () => {
+    
   });
 
 });
-
-// TODO: https://github.com/clearhead/optimizely-snippets/blob/master/tests/GetParamSpec.js
-// describe('experiment/get-param.js', function() {
-//   it('can get params', function() {
-//     expect(window.clearhead.getParam('p', '?p=v')).toBe('v');
-//   });
-// });
