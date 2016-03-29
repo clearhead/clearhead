3062775338f7680f839ddc81fcd8616de39a1568
Troy Acosta <troyacosta@sbcglobal.net>
2016-03-29 11:20:08 -0500

    added test for when optional search argument is passed

--- test/get-param.js
+++ test/get-param.js
@@ -1,10 +1,16 @@
 import chai, { assert, expect } from 'chai';
 import getParam from '../src/get-param';
 
-describe('#getParam()', () => {
+describe('#getParam()', function() {
 
-  it('should return the value of a key given a URL string with query parameters', () => {
-    
+	it('should exist', function() {
+		expect(getParam).not.to.be.undefined;
+	})
+
+  it('should return the value of a key given a URL string with query parameters', function() {
+  	assert.equal(getParam('name', '?name=test'), 'test');
+  	assert.equal(getParam('name', '?name=[t3$ting]'), '[t3$ting]');
+  	assert.equal(getParam('name', '?name=test+test'), 'test test');
   });
 
 });
