4424e57348cf4afc7d814c70535d0caea39be417
Casey White <casey.r.white@gmail.com>
2016-03-29 11:34:35 -0500

    add psuedo code

--- test/get-param.js
+++ test/get-param.js
@@ -3,14 +3,23 @@ import getParam from '../src/get-param';
 
 describe('#getParam()', function() {
 
-	it('should exist', function() {
-		expect(getParam).not.to.be.undefined;
-	})
+  const url = ;
 
-  it('should return the value of a key given a URL string with query parameters', function() {
-  	assert.equal(getParam('name', '?name=test'), 'test');
-  	assert.equal(getParam('name', '?name=[t3$ting]'), '[t3$ting]');
-  	assert.equal(getParam('name', '?name=test+test'), 'test test');
+  // it('should return the value of a key given a URL string with query parameters', function() {
+  // 	assert.equal(getParam('name', '?name=test'), 'test');
+  // 	assert.equal(getParam('name', '?name=[t3$ting]'), '[t3$ting]');
+  // 	assert.equal(getParam('name', '?name=test+test'), 'test test');
+  // });
+
+  it('should return the value of a key given a URL string with query parameters if that key exists in the query string', function() {
+
+    const name = getParam('name', url);
+  	assert.equal(name, 'test');
+  });
+
+  it('should return an empty string if the given key is not present in the query string', function() {
+  	const empty = getParam('empty', url);
+    assert.equal(empty, '');
   });
 
 });
