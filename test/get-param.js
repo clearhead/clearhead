a9b4e57420470024564a4573ac75164e8b1e652b
Troy Acosta <troyacosta@sbcglobal.net>
2016-03-30 11:45:47 -0500

    added test that checks to see if '+' is replaced with a space in key values

--- test/get-param.js
+++ test/get-param.js
@@ -3,18 +3,16 @@ import getParam from '../src/get-param';
 
 describe('#getParam()', function() {
 
-  const url = ;
-
-  // it('should return the value of a key given a URL string with query parameters', function() {
-  // 	assert.equal(getParam('name', '?name=test'), 'test');
-  // 	assert.equal(getParam('name', '?name=[t3$ting]'), '[t3$ting]');
-  // 	assert.equal(getParam('name', '?name=test+test'), 'test test');
-  // });
+  const url = 'https://www.optimizely.com/partners/solutions/clearhead/?name=test&title=foo+bar';
 
   it('should return the value of a key given a URL string with query parameters if that key exists in the query string', function() {
-
     const name = getParam('name', url);
   	assert.equal(name, 'test');
+  });
+
+   it('if the value of a given key contains a "+", it should be returned with the "+" replaced with a space', function() {
+    const title = getParam('title', url);
+  	assert.equal(title, 'foo bar');
   });
 
   it('should return an empty string if the given key is not present in the query string', function() {
