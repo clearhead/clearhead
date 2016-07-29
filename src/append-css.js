4a48ac097d238f560f158d754a1d9cd34bdb668e
tomhanlon <tomhanlon@gmail.com>
2016-07-29 10:13:24 -0400

    Update function to no longer wait until document head has finished loading

--- src/append-css.js
+++ src/append-css.js
@@ -4,14 +4,10 @@
  *
  * @param {String} string of css rules, with or without style tags
  */
-import when from './when';
-
 function appendCss(css){
   if (css.indexOf('<style>') !== 0 ) css = css.replace(/^/,'<style>');
   if (css.indexOf('</style>') === -1 ) css = css.replace(/$/,'</style>');
-  when('body', function(){
-    document.head.insertAdjacentHTML('beforeend', css);
-  });
+  document.head.insertAdjacentHTML('beforeend', css);
 }
 
 export default appendCss;
