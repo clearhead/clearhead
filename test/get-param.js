f201cf85457fbb4d852e6f26b144de9fd21a4d5d
Aaron Montana <aaronmontana@gmail.com>
2016-09-15 16:43:01 -0500

    getParams Test  Adding tests to learn about the CH library   - Simple param stirng testing  Todo:   - Testing null, NaN, undefined   - Injecting window.location.search with headless test

--- test/get-param.js
+++ test/get-param.js
@@ -1,11 +1,26 @@
 import chai, { assert, expect } from 'chai';
-import curry from '../src/get-param';
+import getParam from '../src/get-param';
 
 describe('#getParam()', () => {
 
-  it('should getParam a thing', () => {
-    assert.equal('tomf', 'tomf'); // 'good person');
-  });
+	it('Find an animal in a query string', () => {
+		var search = "?utf8=%E2%9C%93&animals=otters";
+		assert.equal(getParam('animals', search), 'otters', "Found the otter.");
+		assert.notEqual(getParam('Animals', search), 'otters', "Key is capitalized.");
+		assert.notEqual(getParam('animals', search), 'Otters', "Param value is capitalized.");
+		assert.notEqual(getParam('Animals', search), 'Otters', "Key and Param value is capitalized.");
+		assert.notEqual(getParam('animals', search), 'kitty', "Param value is wrong.");
+	});
+
+	it('Find animals in a query string', () => {
+		var search = "?utf8=%E2%9C%93&animals=otters+dogs";
+		assert.equal(getParam('animals', search), 'otters dogs', "Found the otter and dog.");
+		assert.notEqual(getParam('animals', search), 'otters+dogs', "Test will parse out the string concat.");
+		assert.notEqual(getParam('Animals', search), 'otters dogs', "Key is capitalized.");
+		assert.notEqual(getParam('animals', search), 'Otters dogs', "Param value is capitalized.");
+		assert.notEqual(getParam('Animals', search), 'Otters dogs', "Key and Param value is capitalized.");
+		assert.notEqual(getParam('animals', search), 'kitty dogs', "Param value is wrong.");
+	});
 
 });
 
